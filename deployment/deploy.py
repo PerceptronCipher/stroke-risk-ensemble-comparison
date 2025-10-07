be43b242642485d7171f814ee9db83fa3e8d9a6a
Adeyemi <adeyemiboluwatife.olayinka@gmail.com>
2025-10-07 09:33:50 +0100

    Update deploy.py
    update filepath

--- deployment/deploy.py
+++ deployment/deploy.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 
 # Load model
-model = joblib.load(r"models\best_model.pkl")
+model = joblib.load("models/best_model.pkl")
 
 # App title
 st.title("🏥 Stroke Risk Prediction")
